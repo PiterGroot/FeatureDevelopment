adf502dd31a414d569e50eba7bdb81afd08ca5e0
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-09-17 15:02:37 +0200

    Tiles gemaakt

---
+++ Assets/Tile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField]private bool _isBuildable;
+    public bool GetIsBuildable() {
+        return _isBuildable;
+    }
+}
